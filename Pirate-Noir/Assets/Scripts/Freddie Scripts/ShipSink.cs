669e920d15e6f86ef639861f59ffcba51100bccc
tiptonblu3 <tiptonblu3@gmail.com>
2026-07-02 16:18:52 -0400

    Enemy Waves (Fix)
    Fixed the Enemy Waves in Level 1 there was a second wave spawner that made too many enemies. Started the Ship Sink and brought in models.

---
+++ Pirate-Noir/Assets/Scripts/Freddie Scripts/ShipSink.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ShipSink : MonoBehaviour
+{
+    public bool Sinking;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
